b13eff82f0da78b0498af0b96f9d87b0221dd230
linus muema <linus.m.muema@gmail.com>
2020-08-25 13:16:17 +0300

    - added the clickListeners

--- app/src/main/java/com/one46health/medis/ui/MainActivity.kt
+++ app/src/main/java/com/one46health/medis/ui/MainActivity.kt
@@ -1,46 +1,22 @@
 package com.one46health.medis.ui
 
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
-import android.widget.GridView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.one46health.medis.R
-import com.one46health.medis.adapters.MainCardAdapter
-import com.one46health.medis.model.MainCardItem
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
-
-    private var arrayList: ArrayList<MainCardItem>? = null
-    private var gridView: GridView? = null
-    private var mainCardAdapter: MainCardAdapter? = null
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        gridView = findViewById(R.id.main_gridview_list)
-        arrayList = ArrayList()
-        arrayList = setMainTabs()
-        mainCardAdapter = MainCardAdapter(applicationContext, arrayList!!)
-        gridView?.adapter = mainCardAdapter
-        gridView?.onItemClickListener = this
+        house_hold.setOnClickListener { Toast.makeText(this,"House Hold", Toast.LENGTH_SHORT).show() }
+        referral.setOnClickListener { Toast.makeText(this,"Referral", Toast.LENGTH_SHORT).show() }
+        reports.setOnClickListener { Toast.makeText(this,"Reports", Toast.LENGTH_SHORT).show() }
+        e_learning.setOnClickListener { Toast.makeText(this,"E-learning", Toast.LENGTH_SHORT).show() }
+        events.setOnClickListener { Toast.makeText(this,"Events", Toast.LENGTH_SHORT).show() }
     }
 
-    private fun setMainTabs(): ArrayList<MainCardItem> {
-        var arrayList: ArrayList<MainCardItem> = ArrayList()
-        arrayList.add(MainCardItem(R.drawable.house_hold, "House Holds"))
-        arrayList.add(MainCardItem(R.drawable.refferal_img, "Referrals"))
-        arrayList.add(MainCardItem(R.drawable.report_img, "Reports"))
-        arrayList.add(MainCardItem(R.drawable.elearning_img, "E-Learning"))
-        arrayList.add(MainCardItem(R.drawable.events_img, "Events"))
-
-        return arrayList
-    }
-
-    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        var items: MainCardItem = arrayList!!.get(position)
-        Toast.makeText(applicationContext, items.name, Toast.LENGTH_LONG).show()
-    }
 }
