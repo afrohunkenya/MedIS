7c1a4f36249c4ed79331ce7aa35a7e535240739f
sklagat45 <sklagat46@gmail.com>
2020-08-28 00:01:01 +0300

    updated refactored db

---
+++ app/src/main/java/com/one46health/medis/ui/IndividualHouseData.kt
@@ -0,0 +1,12 @@
+package com.one46health.medis.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.one46health.medis.R
+
+class IndividualHouseData : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_individual_house_data)
+    }
+}
