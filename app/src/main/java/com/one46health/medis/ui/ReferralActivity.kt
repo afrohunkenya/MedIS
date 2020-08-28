413b4f5237282cc7ebe8ccdfeb1db76271c57cc2
sklagat45 <sklagat46@gmail.com>
2020-08-28 04:26:22 +0300

    feature-jontes-data Created empty activities

---
+++ app/src/main/java/com/one46health/medis/ui/ReferralActivity.kt
@@ -0,0 +1,11 @@
+package com.one46health.medis.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReferralActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_referrel)
+    }
+}
---
+++ app/src/main/java/com/one46health/medis/ui/ReportsActivity.kt
@@ -0,0 +1,12 @@
+package com.one46health.medis.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.one46health.medis.R
+
+class ReportsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reports)
+    }
+}
