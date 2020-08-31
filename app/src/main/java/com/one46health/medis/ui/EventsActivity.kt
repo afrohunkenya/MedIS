2772a2cc8ac63f8c7d9039b047b3b5cee98b8863
sklagat45 <sklagat46@gmail.com>
2020-08-31 17:53:30 +0300

    feature addIndividualHouseData.kt

--- app/src/main/java/com/one46health/medis/ui/EventsActivity.kt
+++ app/src/main/java/com/one46health/medis/ui/EventsActivity.kt
@@ -2,11 +2,14 @@ package com.one46health.medis.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import com.one46health.medis.R
+import kotlinx.android.synthetic.main.activity_events.*
 
 class EventsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_events)
+        event_link_one!!.movementMethod = LinkMovementMethod.getInstance()
     }
 }
